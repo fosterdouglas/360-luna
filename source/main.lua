925549b3434a099dbfb6eb5126633320564dad41
Foster Douglas <foster@sandcastle.co>
2022-06-15 17:27:56 -0700

    add alt-named moon images, test coroutines

--- source/main.lua
+++ source/main.lua
@@ -34,38 +34,84 @@ gfx.sprite.setBackgroundDrawingCallback(
 
 -- -- -- -- SETUP -- -- -- -- 
 -- playdate.display.setRefreshRate(45)
-local moonTable = gfx.imagetable.new('assets/images/moon/moon')
-local moonImage = moonTable:getImage(1)
+local moonImage
 local moonScale = 1
 local crankPos
 local isScaling = false
 local scaleFactor = 0
 local increaseScaleBy = 0.01
 local decreaseScaleBy = -0.01
+local crankGross = 1
+local crankChange, crankAccChange
+local moonTable
+
+
+-- createImageTable()
+gfx.setLineWidth(5)
+
+local moonTable = gfx.imagetable.new(720)
+local imagesLoaded = false
+
+-- playdate.display.setMosaic(1, 1)
+
+--TODO:
+-- why the coroutine so slow
+-- add some simple stars?
+-- ambient music?
 
 -- -- -- -- UPDATE -- -- -- -- 
 function playdate.update()
-	gfx.sprite.update()
-	
-	crankPos = _roundNumber(playdate.getCrankPosition() * 2, 0)
-	
-	
-	if isScaling then
-		moonScale += scaleFactor
+	if not imagesLoaded then
+		gfx.sprite.update()
 		
-		if moonScale < 0.05 then
-			moonScale = 0.05
+		-- Load Images
+		for i=1, 720 do
+			local progress = i/720
+			local path = gfx.image.new('assets/images/moon-test/moon' .. i)
+			moonTable:setImage(i, path)
+			
+			gfx.setColor(kWhite)
+			gfx.drawArc(kDisplayWidth/2, kDisplayHeight/2, 105, 0, playdate.math.lerp(0, 360, progress))
+			gfx.setColor(kWhite)
+			gfx.setDitherPattern(playdate.math.lerp(1, 0.5, progress))
+			gfx.fillCircleAtPoint(kDisplayWidth/2, kDisplayHeight/2, 100)
+			
+			coroutine.yield()
 		end
-	end
-	
-	if crankPos == 0.0 then
-		moonImage = moonTable:getImage(1)
-		moonImage = moonImage:scaledImage(moonScale)
-		moonImage:drawCentered(kDisplayWidth/2, kDisplayHeight/2)
+		
+		imagesLoaded = true
+		
 	else
-		moonImage = moonTable:getImage(crankPos)
-		moonImage = moonImage:scaledImage(moonScale)
-		moonImage:drawCentered(kDisplayWidth/2, kDisplayHeight/2)
+	
+		gfx.sprite.update()
+		
+		crankChange, crankAccChange = playdate.getCrankChange()
+		crankGross = crankGross + (crankAccChange * 4)
+		
+		crankPos = _roundNumber(crankGross, 0)%720
+		
+		
+		if isScaling then
+			moonScale += scaleFactor
+			
+			-- move this into input handler
+			if moonScale < 0.25 then
+				moonScale = 0.25
+			elseif moonScale > 2 then
+				moonScale = 2
+			end
+		end
+		
+		if crankPos == 0.0 then
+			moonImage = moonTable:getImage(1)
+			moonImage = moonImage:scaledImage(moonScale)
+			moonImage:drawCentered(kDisplayWidth/2, kDisplayHeight/2)
+		else
+			moonImage = moonTable:getImage(crankPos)
+			moonImage = moonImage:scaledImage(moonScale)
+			moonImage:drawCentered(kDisplayWidth/2, kDisplayHeight/2)
+		end
+		
 	end
 end
 
@@ -88,7 +134,8 @@ function playdate.downButtonUp()
 	isScaling = false
 end
 
-function scaleMoon()
+function playdate.BButtonDown()
+	moonScale = 1
 end
 
 	-- -- -- -- DEBUG -- -- -- --
